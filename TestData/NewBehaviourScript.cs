6179d16a9fca9db0ee10a61bfbd1aac3178c42dd
olegchumakov <zulman@bk.ru>
2014-06-19 20:38:14 +0400

    upload

---
+++ TestData/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+        Debug.Log("NBH Start");
+    }
+    void Awake () {
+        Debug.Log("NBH Awake");
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
